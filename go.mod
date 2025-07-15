ad770a2a6168ea3cf6d5413ca855e1be01bcf497
jpgtzg <58833781+jpgtzg@users.noreply.github.com>
2025-07-14 21:00:37 -0600

    chore: update dependencies and enhance main.go with VapiAI client integration

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module sarah
 
 go 1.24.4
+
+require (
+	github.com/VapiAI/server-sdk-go v0.9.0 // indirect
+	github.com/google/uuid v1.4.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	vapiclient "github.com/VapiAI/server-sdk-go/client"
+	"github.com/VapiAI/server-sdk-go/option"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	// load .env
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// Create client with API key and custom timeout
+	client := vapiclient.NewClient(
+		option.WithToken(os.Getenv("VAPI_API_KEY")),
+		option.WithHTTPClient(
+			&http.Client{
+				Timeout: 5 * time.Second,
+			}),
+	)
+
+	fmt.Println(client)
 }
